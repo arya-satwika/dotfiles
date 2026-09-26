42d8f5cd56a6580d18158113f45433125ca7891a
arya-satwika <aryastwk95@gmail.com>
2026-09-26 21:47:10 +0700

    l command shows full size of dir

--- fish/.config/fish/functions/_fish_eza_l.fish
+++ fish/.config/fish/functions/_fish_eza_l.fish
@@ -1,4 +1,4 @@
 # List files respecting .gitignore
-function _fish_eza_l --wraps _fish_eza_ls
-    _fish_eza_ls --git-ignore $argv
+function _fish_eza_l --wraps _fish_eza_ll
+    _fish_eza_ll --git-ignore --total-size $argv
 end
